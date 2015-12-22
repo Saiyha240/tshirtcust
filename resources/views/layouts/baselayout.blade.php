f1f0826f63afcfa2ecb1a5d4db53cebcc1af6b3b
Mark <mark5cinco92@gmail.com>
2015-12-22 22:34:23 +0800

    Changed to CDN for the meantime

--- resources/views/layouts/baselayout.blade.php
+++ resources/views/layouts/baselayout.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Custom Tshirt</title>
-	{!! HTML::style('css/bootstrap.css') !!}
+	{!! HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
 	{!! HTML::style('css/style.css') !!}
 </head>
 <body>
@@ -12,7 +12,7 @@
 
 @include('layouts.partials.footer')
 </body>
-{!! HTML::script('js/jquery.min.js') !!}
-{!! HTML::script('js/bootstrap.min.js') !!}
+{!! HTML::script('https://code.jquery.com/jquery-1.11.3.min.js') !!}
+{!! HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js') !!}
 {!! HTML::script('js/scripts.js') !!}
 </html>
